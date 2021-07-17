7f41a7162cd7a019a46874d11ee1533e67096692
Rafael Batista <maranata_rafa@hotmail.com>
2021-07-17 18:31:45 -0400

    ensure AuthenticationHandler calls HttpClient with correct body

--- test/business/services/authentication_test.dart
+++ test/business/services/authentication_test.dart
@@ -2,19 +2,27 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:enquetes_flutter_mango/domain/useCases/authentication.dart';
+
 class AuthenticationHandler {
   final HttpClient httpClient;
   final String url;
 
-  AuthenticationHandler({required this.httpClient, required this.url});
+  AuthenticationHandler({
+    required this.httpClient,
+    required this.url});
 
-  Future<void> auth() async {
-    await httpClient.request(url: url, method: "post");
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {"email": params.email, "password": params.secret};
+    await httpClient.request(url: url, method: "post", body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url, required String method});
+  Future<void>? request({
+    required String url,
+    required String method,
+    Map<String, dynamic> body});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -37,8 +45,13 @@ void main() {
   });
 
   test("Should call HttpClient with correct values", () async {
-    await make.sut.auth();
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
 
-    verify(make.httpClient.request(url: make.url, method: "post"));
+    await make.sut.auth(params);
+
+    verify(make.httpClient.request(
+        url: make.url,
+        method: "post",
+        body: {"email": params.email, "password": params.secret}));
   });
 }
