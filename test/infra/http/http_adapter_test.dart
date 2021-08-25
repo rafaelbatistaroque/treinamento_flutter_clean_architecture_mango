62afd86df8112536e6d9678fc86c1edbc799ffd7
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-24 21:04:45 -0400

    ensure HttpAdapter returns null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -11,13 +11,13 @@ class HttpAdapter implements HttpClient {
 
   HttpAdapter(this.client);
 
-  Future<Map> request({required String url, required String method, Map? body}) async {
+  Future<Map?> request({required String url, required String method, Map? body}) async {
     final headers = {"content-type": "application/json", "accept": "application/json"};
     final jsonBody = body != null ? jsonEncode(body) : null;
 
     final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
 
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -64,6 +64,14 @@ void main() {
       final response = await sut.request(url: url, method: "post");
 
       expect(response, {"any_key": "any_value"});
+    });
+
+    test("Should return null if post returns 200 with no data", () async {
+      when(() => client.post(any(), headers: any(named: "headers"))).thenAnswer((_) async => Response("", 200));
+
+      final response = await sut.request(url: url, method: "post");
+
+      expect(response, null);
     });
   });
 }
