c52e291127df69710820aabe7c0601284cf8142c
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-24 22:19:43 -0400

    ensure httpAdapter throws serverError if post throws

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -12,7 +12,11 @@ class HttpAdapter implements HttpClient {
     final headers = {"content-type": "application/json", "accept": "application/json"};
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response("", 500);
-    if (method == "post") response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    try {
+      if (method == "post") response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    } catch (error) {
+      throw HttpError.serverError;
+    }
 
     return _handleResponse(response);
   }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -27,10 +27,12 @@ void main() {
     });
   });
   group("post", () {
-    mockRequest() => when(() => client.post(any(), headers: any(named: "headers"), body: any(named: "body")));
-    void mockResponse(int statusCode, {String body = '{"any_key": "any_value"}'}) {
-      mockRequest().thenAnswer((_) async => Response(body, statusCode));
-    }
+    When mockRequest() => when(() => client.post(any(), headers: any(named: "headers"), body: any(named: "body")));
+
+    void mockResponse(int statusCode, {String body = '{"any_key": "any_value"}'}) =>
+        mockRequest().thenAnswer((_) async => Response(body, statusCode));
+
+    void mockError() => mockRequest().thenThrow(Exception());
 
     setUp(() {
       mockResponse(200);
@@ -126,6 +128,14 @@ void main() {
 
     test("Should return ServerError if post returns 500", () async {
       mockResponse(500);
+
+      final future = sut.request(url: url, method: "post");
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test("Should return ServerError if post throws", () async {
+      mockError();
 
       final future = sut.request(url: url, method: "post");
 
