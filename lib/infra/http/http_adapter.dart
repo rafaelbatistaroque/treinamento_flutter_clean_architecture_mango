230fb8663e8c9496da376024ee511d9590520d20
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-24 21:45:11 -0400

    move return response to private handler response

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -11,10 +11,13 @@ class HttpAdapter implements HttpClient {
   Future<Map?> request({required String url, required String method, Map? body}) async {
     final headers = {"content-type": "application/json", "accept": "application/json"};
     final jsonBody = body != null ? jsonEncode(body) : null;
-
     final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    if (response.statusCode == 200) return response.body.isEmpty ? null : jsonDecode(response.body);
 
+    return _handleResponse(response);
+  }
+
+  Map? _handleResponse(Response response) {
+    if (response.statusCode == 200) return response.body.isEmpty ? null : jsonDecode(response.body);
     return null;
   }
 }
