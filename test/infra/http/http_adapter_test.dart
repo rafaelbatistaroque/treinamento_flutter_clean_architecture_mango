206ca98cf5f2497210eddedaf337bc29f295e8fd
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-24 21:40:59 -0400

    move httpAdapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,26 +1,10 @@
 import 'dart:convert';
 
-import 'package:enquetes_flutter_mango/business/contract/contract.dart';
+import 'package:enquetes_flutter_mango/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map?> request({required String url, required String method, Map? body}) async {
-    final headers = {"content-type": "application/json", "accept": "application/json"};
-    final jsonBody = body != null ? jsonEncode(body) : null;
-
-    final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    if (response.statusCode == 200) return response.body.isEmpty ? null : jsonDecode(response.body);
-
-    return null;
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
