8a567b1bb22b6bcc31ea0be0a171d6e7e090e378
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-26 19:57:22 -0400

    ensure StreamLoginPresenter emits error if email is invalid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,18 +1,30 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 abstract class Validation {
-  String? validate({required String field, required String value});
+  String validate({required String field, required String value});
+}
+
+class LoginState {
+  late String emailError;
 }
 
 class StreamLoginPresenter {
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: "email", value: email);
+    _state.emailError = validation.validate(field: "email", value: email);
+    _controller.add(_state);
   }
 }
 
@@ -30,8 +42,18 @@ void main() {
   });
 
   test("Should call Validation with correct email", () {
+    when(() => validation.validate(field: any(named: "field"), value: any(named: "value"))).thenReturn("");
+
     sut.validateEmail(email);
 
     verify(() => validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test("Should emit email error if validate fails", () {
+    when(() => validation.validate(field: any(named: "field"), value: any(named: "value"))).thenReturn("error");
+
+    expectLater(sut.emailErrorStream, emits("error"));
+
+    sut.validateEmail(email);
   });
 }
