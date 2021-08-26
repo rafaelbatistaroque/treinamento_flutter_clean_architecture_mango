caa3de2b37050380e952fed239e3a2d1b06a7138
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-26 15:59:23 -0400

    ensure LoginPage present loading

--- lib/ui/pages/login/login_pages.dart
+++ lib/ui/pages/login/login_pages.dart
@@ -12,63 +12,91 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            LoginHeader(),
-            Text(
-              "Login".toUpperCase(),
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32),
-              child: Form(
-                child: Column(
-                  children: [
-                    StreamBuilder<String>(
-                        stream: presenter!.emailErrorStream,
-                        builder: (context, snapshot) {
-                          return TextFormField(
-                              onChanged: presenter!.validateEmail,
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: InputDecoration(
-                                labelText: "E-mail",
-                                icon: Icon(Icons.email, color: AppColor.primaryColorLight),
-                                errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
-                              ));
-                        }),
-                    SizedBox(height: 20),
-                    StreamBuilder<String>(
-                        stream: presenter!.passwordErrorStream,
-                        builder: (context, snapshot) {
-                          return TextFormField(
-                            onChanged: presenter!.validatePassword,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: "Senha",
-                              icon: Icon(Icons.lock, color: AppColor.primaryColorLight),
-                              errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
-                            ),
-                          );
-                        }),
-                    SizedBox(height: 30),
-                    StreamBuilder<bool>(
-                        stream: presenter!.isFormValidStream,
-                        builder: (context, snapshot) {
-                          return ElevatedButton(
-                            onPressed: snapshot.data == true ? presenter!.auth : null,
-                            child: Text("Entrar".toUpperCase()),
-                          );
-                        }),
-                    TextButton.icon(onPressed: () {}, label: Text("Entrar".toUpperCase()), icon: Icon(Icons.person))
-                  ],
+      body: Builder(
+        builder: (context) {
+          presenter!.isLoadingStream.listen((isLoading) {
+            if (isLoading)
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) => SimpleDialog(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CircularProgressIndicator(),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "Aguarde...",
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          )
+                        ],
+                      ));
+          });
+
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                LoginHeader(),
+                Text(
+                  "Login".toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline1,
                 ),
-              ),
-            )
-          ],
-        ),
+                Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        StreamBuilder<String>(
+                            stream: presenter!.emailErrorStream,
+                            builder: (context, snapshot) {
+                              return TextFormField(
+                                  onChanged: presenter!.validateEmail,
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                    labelText: "E-mail",
+                                    icon: Icon(Icons.email, color: AppColor.primaryColorLight),
+                                    errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
+                                  ));
+                            }),
+                        SizedBox(height: 20),
+                        StreamBuilder<String>(
+                            stream: presenter!.passwordErrorStream,
+                            builder: (context, snapshot) {
+                              return TextFormField(
+                                onChanged: presenter!.validatePassword,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  labelText: "Senha",
+                                  icon: Icon(Icons.lock, color: AppColor.primaryColorLight),
+                                  errorText: snapshot.data?.isEmpty == true ? null : snapshot.data,
+                                ),
+                              );
+                            }),
+                        SizedBox(height: 30),
+                        StreamBuilder<bool>(
+                            stream: presenter!.isFormValidStream,
+                            builder: (context, snapshot) {
+                              return ElevatedButton(
+                                onPressed: snapshot.data == true ? presenter!.auth : null,
+                                child: Text("Entrar".toUpperCase()),
+                              );
+                            }),
+                        TextButton.icon(onPressed: () {}, label: Text("Entrar".toUpperCase()), icon: Icon(Icons.person))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -12,15 +12,18 @@ main() {
   late StreamController<String> emailErrorController;
   late StreamController<String> passwordErrorController;
   late StreamController<bool> isFormValidController;
+  late StreamController<bool> isLoadingController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
     emailErrorController = StreamController<String>();
     passwordErrorController = StreamController<String>();
     isFormValidController = StreamController<bool>();
+    isLoadingController = StreamController<bool>();
     when(() => presenter.emailErrorStream).thenAnswer((_) => emailErrorController.stream);
     when(() => presenter.passwordErrorStream).thenAnswer((_) => passwordErrorController.stream);
     when(() => presenter.isFormValidStream).thenAnswer((_) => isFormValidController.stream);
+    when(() => presenter.isLoadingStream).thenAnswer((_) => isLoadingController.stream);
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
@@ -29,6 +32,7 @@ main() {
     emailErrorController.close();
     passwordErrorController.close();
     isFormValidController.close();
+    isLoadingController.close();
   });
 
   testWidgets("Should load with correct initial state", (WidgetTester tester) async {
@@ -149,5 +153,14 @@ main() {
     await tester.pump();
 
     verify(() => presenter.auth()).called(1);
+  });
+
+  testWidgets("Should present loading", (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 }
