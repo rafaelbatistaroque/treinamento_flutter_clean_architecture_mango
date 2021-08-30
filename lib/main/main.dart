e0e7d67412ffc98d91a19b11babdeafc402a1c70
Rafael Batista <maranata_rafa@hotmail.com>
2021-08-29 21:33:17 -0400

    add fallback to conflicts between providers Getx and Provider

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import './factories/factories.dart';
+import '../../ui/pages/pages.dart';
 import '../../ui/components/components.dart';
 
-void main() => runApp(App());
+void main() {
+  //Invalida o provider do GETX
+  final previous = Provider.debugCheckInvalidValueType;
+  Provider.debugCheckInvalidValueType = <T>(value) {
+    if (value is LoginPresenter) return;
+    previous!<T>(value);
+  };
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
